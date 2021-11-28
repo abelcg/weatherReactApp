65ff857ba4358886a7cff2458d8e82477f1b7863
abelcg <abelcg86@gmail.com>
2021-11-27 23:52:09 -0300

    se agrego la logica de consulta a la API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,72 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Forms from './components/Forms';
-import WheatherContainer from './components/WheatherContainer';
-
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Forms from "./components/Forms";
+import WheatherContainer from "./components/WheatherContainer";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [climate, setClimate] = useState({});
+  const [icon, setIcon] = useState("");
+
+  useEffect(() => {
+    getWeather();
+    getWeatherIcon();
+  },[]);
+
+  let getWeather = async () => {
+    const apiCall = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=Paris,fr&units=metric&APPID=5f87bfcc54a9a5edbfcfd70e3e88febf&lang=es"
+    );
+    const response = await apiCall.json();
+    console.log(response.weather[0].main);
+    setClimate(response);
+  };
+
+  const weatherIcon = {
+    thunderstorm: "thunderstorm",
+    drizzle: "sleet",
+    rain: "storm-showers",
+    snow: "snow",
+    atmosphere: "fog",
+    clear: "sunny",
+    clouds: "cloudy",
+  };
+
+  let getWeatherIcon = () => {
+    switch (climate.weather[0].main) {
+      case "Thunderstorm":
+        setIcon(weatherIcon.thunderstorm);
+        break;
+      case "Drizzle":
+        setIcon(weatherIcon.drizzle);
+        break;
+
+      case "Rain":
+        setIcon(weatherIcon.rain);
+        break;
+      case "Snow":
+        setIcon(weatherIcon.snow);
+        break;
+      case "Atmosphere":
+        setIcon(weatherIcon.atmosphere);
+        break;
+      case "Clear":
+        setIcon(weatherIcon.clear);
+        break;
+      case "Clouds":
+        setIcon(weatherIcon.clouds);
+        break;
+
+      default:
+        setIcon(weatherIcon.clear);
+        break;
+    }
+  };
+
   return (
     <div className="App">
-     <Forms></Forms>
-     <WheatherContainer></WheatherContainer>
+      <Forms></Forms>
+      <WheatherContainer climate={climate} icon={icon}></WheatherContainer> 
     </div>
   );
 }
--- src/components/WheatherContainer.js
+++ src/components/WheatherContainer.js
@@ -2,33 +2,55 @@ import React from "react";
 import "weather-icons/css/weather-icons.css";
 import "weather-icons/css/weather-icons-wind.css";
 
-const WheatherContainer = () => {
+const WheatherContainer = (props) => {
+ 
   return (
     <section className="container  text-center wheatherContainer py-5">
-      <h1 className="text-light">Buenos Aires, AR</h1>
+      <h1 className="text-light">
+        {props.climate.name}, {props.climate.sys.country}{" "}
+      </h1>
       <hr className="text-light" />
-      <i class="text-light my-5 weatherIcon wi wi-day-sunny"></i>
+      <i className={`text-light my-5 weatherIcon wi wi-night-${props.icon}`}></i>
       <div className="row d-flex justify-content-sm-center align-items-sm-center">
         <div className="col-sm-1">
-          <h2 className="text-light fs-1">33°</h2>
+          <h2 className="text-light fs-1">
+            {Math.floor(props.climate.main.temp)}°C
+          </h2>
         </div>
         <div className="col-sm-1  d-md-flex flex-column">
-          <span className="fw-bold mx-2 text-light fs-6">35°</span>
+          <span className="fw-bold mx-2 text-light fs-6">
+            {Math.floor(props.climate.main.temp_max)}°
+          </span>
           <span className="text-light fs-6">|</span>
-          <span className="text-muted mx-2 text-light fs-6">23°</span>
+          <span className="text-muted mx-2 text-white fs-6">
+            {Math.floor(props.climate.main.temp_min)}°
+          </span>
         </div>
       </div>
-      <h3 className="text-light my-4">Nublado</h3>
+      <h3 className="text-light my-4">
+        {props.climate.weather[0].description}
+      </h3>
       <div className="d-flex flex-row flex-wrap justify-content-around">
-        <p className="lead fs-6 text-light">Sensación térmica: 33°</p>
-        <p className="lead fs-6 text-light">Humedad: 60%</p>
-        <p className="lead fs-6 text-light">Visibilidad: 8km</p>
+        <p className="lead fs-6 text-light">
+          Sensación térmica: {Math.floor(props.climate.main.feels_like)}°C
+        </p>
+        <p className="lead fs-6 text-light">
+          Humedad: {props.climate.main.humidity}%
+        </p>
+        <p className="lead fs-6 text-light">
+          Visibilidad: {props.climate.visibility / 1000}km
+        </p>
       </div>
       <div className="d-flex flex-row flex-wrap justify-content-around">
-        <p className="lead fs-6 text-light">Presión atmosférica: 1000mbar</p>
         <p className="lead fs-6 text-light">
-          Viento: <i class="text-light fs-2 mx-2 wi wi-wind towards-23-deg"></i>
-          13km/h
+          Presión atmosférica: {props.climate.main.pressure}mbar
+        </p>
+        <p className="lead fs-6 text-light">
+          Viento:{" "}
+          <i
+            className={`text-light fs-2 mx-2 wi wi-wind towards-${props.climate.wind.deg}-deg`}
+          ></i>
+          {Math.floor(props.climate.wind.speed * 3.6)}km/h
         </p>
       </div>
     </section>
